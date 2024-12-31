941050d8f2d8b806ee9291e3dc38c911797a2ac0
alexwebdev <lavrovweb@gmail.com>
2024-12-31 18:55:29 +0300

    uniq array with set

---
+++ home-work-1/main.js
@@ -0,0 +1,22 @@
+
+//Уникализировать массив объектов с помощью Set
+
+//Я не совсем понял как правильно сделать с помощью map и find
+// поэтому сделал с filter и findIndex
+
+let arr = [
+    {id:1 , name: 'Vasy'},
+    {id:2, name: 'Petya'},
+    {id:3, name: 'Vladimir'},
+    {id:2, name: 'Sasha'},
+    {id:4, name: 'Vova'}
+]
+
+
+let newArr = new Set([...arr].filter((item, index, array) => {
+    return index === array.findIndex((el) => el.id === item.id);
+}))
+
+newArr.add({id: 5, name: 'Dima'})
+
+console.log(newArr);
